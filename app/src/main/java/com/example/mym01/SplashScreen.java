b78f8674f7ec523d8ff703e41e260f90b0cc4e5b
Nazrul Islam <nazruldiu26@gmail.com>
2019-07-06 16:11:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mym01/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.mym01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //runProgressBar();
+            }
+        });
+        thread.start();
+
+        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+//    public void runProgressBar(){
+//        for (int progress=20;progress<=100; progress=progress+20)
+//        try {
+//            Thread.sleep(1000);
+//            progressBar.setProgress(progress);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
