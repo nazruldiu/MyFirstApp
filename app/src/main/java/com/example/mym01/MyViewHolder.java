e7c4f1bf1c4b70a882e0191431de966699949cb7
Nazrul Islam <nazruldiu26@gmail.com>
2019-07-10 22:41:06 +0600

    added recycle view

---
+++ app/src/main/java/com/example/mym01/MyViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.mym01;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
